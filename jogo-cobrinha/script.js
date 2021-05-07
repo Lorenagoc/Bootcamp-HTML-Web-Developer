c1190d7baa822024e0035ee9015a207f90b05687
Lorenagoc <lorenagoc99@gmail.com>
2021-05-07 16:33:13 -0300

    Criando o movimento da cobrinha

--- jogo-cobrinha/script.js
+++ jogo-cobrinha/script.js
@@ -6,6 +6,8 @@ snake[0] = { //tamanho inicial
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
 function criarBG() {
     context.fillStyle = "lightgreen"; //cor do background do canvas
     context.fillRect(0, 0, 16 * box, 16 * box); //vai desenhar o quadrado
@@ -14,9 +16,33 @@ function criarBG() {
 function criarCobrinha() {
     for (var i = 0; i < snake.length; i++) {
         context.fillStyle = "green"; //cor da cobra
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box); //posicao inicial da cobra
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    /*ponto de partida*/
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    /*pra que onde vai a cobrinha?*/
+    if (direction == "rigth") snakeX += box; //acrescenta um quadradinho
+    if (direction == "left") snakeX -= box; //decrementa um quadradinho
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop(); //retira o ultimo elemento do array
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //insere um elemento no comeco do array
+}
+
+let jogo = setInterval(iniciarJogo, 100); //a cada 100ms a funcao sera renovada
+
